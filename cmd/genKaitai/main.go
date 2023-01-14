e19e709c622f2d5240bb712b164b09f04064fc99
hello <122643035+funplay133@users.noreply.github.com>
2023-01-15 00:10:18 +0200

    improve kaitai gen tool

--- cmd/genKaitai/main.go
+++ cmd/genKaitai/main.go
@@ -1,14 +1,53 @@
+// package genKantai is meant to simplify work
+// with kaitai struct generation.
+//
+// Simply call genKaitai.exe to generate code for ALL
+// templates in `./templates/` folder.
+//
+// Alternatively, you can provide an argument with path to file,
+// e.g. `genKaitai.exe TextMap.ksy`
+// Those can be more than one
 package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
 	"path/filepath"
 )
 
 func main() {
-	Generate(filepath.Join("templates", "DialogueData.ksy"))
-	Generate(filepath.Join("templates", "TextMap.ksy"))
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		entries, err := os.ReadDir("templates")
+		if err != nil {
+			log.Fatalf("can't read dir templates: %s\n", err)
+		}
+		args = convertDirEntries("", entries)
+	}
+
+	for _, e := range args {
+		if !filepath.IsAbs(e) {
+			e = filepath.Join("templates", e)
+		}
+		Generate(e)
+	}
+}
+
+func convertDirEntries(pathPrefix string, source []os.DirEntry) []string {
+	result := []string{}
+
+	for _, e := range source {
+		if e.IsDir() {
+			continue
+		}
+
+		result = append(result, filepath.Join(pathPrefix, e.Name()))
+	}
+
+	return result
 }
 
 func Generate(f string) {
